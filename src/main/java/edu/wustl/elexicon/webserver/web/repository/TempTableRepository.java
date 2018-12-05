a2e313b2ed0eb6d553cccd0fc4f3f8e68a68e81e
bl6394 <bjorn.loftis@gmail.com>
2018-12-05 08:13:08 -0600

    temp table test

--- src/main/java/edu/wustl/elexicon/webserver/web/repository/TempTableRepository.java
+++ src/main/java/edu/wustl/elexicon/webserver/web/repository/TempTableRepository.java
@@ -6,10 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class TempTableRepository {
@@ -26,17 +23,10 @@ public class TempTableRepository {
     public void get(){
         System.out.println("Start");
         jdbcTemplate.execute("drop TEMPORARY table if exists bjorntable;");
-        jdbcTemplate.execute("insert into bjorntable values ('test')");
         jdbcTemplate.execute("create temporary table bjorntable (word VARCHAR(50) NOT NULL);");
         System.out.println("After CREATE");
         jdbcTemplate.execute("insert into bjorntable values ('test')");
         System.out.println("After insert");
-        try {
-            Thread.sleep(30000);
-            System.out.println("After sleep");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         return;
     }
 
