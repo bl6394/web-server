54bbd424b05a44bafe189490c7f6361c4e456947
bloftis <bloftis@clearent.com>
2019-04-11 16:38:32 -0500

    email shit

---
+++ src/main/java/edu/wustl/elexicon/webserver/service/Mailer.java
@@ -0,0 +1,95 @@
+package edu.wustl.elexicon.webserver.service;
+
+import edu.wustl.elexicon.webserver.web.controller.Query13Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.activation.DataHandler;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+@Service
+public class Mailer {
+
+    private final String userName;
+    private final String password;
+    private final String server;
+    private final JavaMailSenderImpl mailSender;
+
+    public Mailer(@Value("${mailer.server}") String server, @Value("${mailer.username}") String userName, @Value ("${mailer.password}") String password){
+        this.userName = userName;
+        this.password = password;
+        this.server = server;
+        this.mailSender = getJavaMailSender();
+    }
+
+    private final Logger log = LoggerFactory.getLogger(Query13Controller.class);
+
+
+
+    public void sendMessage(String text, String email){
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(email);
+//        message.setSubject("English Lexicon Project: Trx #");
+//        message.setText("This is a test.");
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        try {
+            MimeBodyPart body = new MimeBodyPart();
+            body.setText("This is a test.");
+            MimeBodyPart attachment = new MimeBodyPart();
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(body);
+            ByteArrayDataSource ds = new ByteArrayDataSource("foo bar", "text/plain");
+            attachment.setDataHandler(new DataHandler(ds));
+            attachment.setFileName("bjorntest.txt");
+            multipart.addBodyPart(attachment);
+            mimeMessage.setContent(multipart);
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
+        }
+
+        MimeMailMessage mmm = new MimeMailMessage(mimeMessage);
+
+        mmm.setTo( email );
+        mmm.setSubject("English Lexicon Project: Trx #");
+        mmm.setFrom("bjorn.loftis@gmail.com");
+
+        mailSender.send(mmm.getMimeMessage());
+    }
+
+
+    private JavaMailSenderImpl getJavaMailSender() {
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(server);
+        mailSender.setPort(587);
+
+        mailSender.setUsername(userName);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
--- src/main/java/edu/wustl/elexicon/webserver/web/controller/Query13Controller.java
+++ src/main/java/edu/wustl/elexicon/webserver/web/controller/Query13Controller.java
@@ -1,8 +1,14 @@
 package edu.wustl.elexicon.webserver.web.controller;
 
+import edu.wustl.elexicon.webserver.service.Mailer;
 import edu.wustl.elexicon.webserver.web.repository.ItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
@@ -11,20 +17,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 @Controller
 public class Query13Controller {
 
+    private final Logger log = LoggerFactory.getLogger(Query13Controller.class);
+
     @Value("${maxquerysize}")
     private Integer maxHtmlResultSet;
 
+    private final Mailer mailer;
     private ItemRepository itemRepository;
 
-    public Query13Controller(ItemRepository itemRepository){
+    public Query13Controller(ItemRepository itemRepository, Mailer mailer) {
         this.itemRepository = itemRepository;
+        this.mailer = mailer;
     }
 
-    @PostMapping (value = "/query13/query13do", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @PostMapping(value = "/query13/query13do", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String process(@RequestBody MultiValueMap<String, String> formData, Model model) {
         String targetDb = formData.get("scope").contains("RESELP") ? "item" : "itemplus";
         List<Map<String, String>> query = itemRepository.get(formData.get("field"), targetDb, formData.get("constraints"));
@@ -34,9 +45,9 @@ public class Query13Controller {
         model.addAttribute("field", formData.get("field"));
         model.addAttribute("items", query);
         model.addAttribute("itemCount", query.size());
-        model.addAttribute("targetDb", formData.get("scope").contains("RESELP") ? "Restricted" : "Complete" );
+        model.addAttribute("targetDb", formData.get("scope").contains("RESELP") ? "Restricted" : "Complete");
         addButtonFlags(formData, model);
-        if (query.size() > maxHtmlResultSet){
+        if (query.size() > maxHtmlResultSet) {
             return "query13/emailresponse";
         }
         if (query.isEmpty()) {
@@ -47,7 +58,7 @@ public class Query13Controller {
         return "query13/query13do";
     }
 
-    @PostMapping (value = "/query13/query13domore", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @PostMapping(value = "/query13/query13domore", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public String processEmail(@RequestBody MultiValueMap<String, String> formData, Model model) {
         String emailAddress = formData.getFirst("address");
         if (emailAddress.isEmpty()) {
@@ -55,8 +66,10 @@ public class Query13Controller {
             return "query13/emailresponse";
         }
         model.addAttribute("emailAddress", emailAddress);
+        mailer.sendMessage(null, emailAddress);
         return "query13/query13doemail";
     }
+
 
     private void addButtonFlags(@RequestBody MultiValueMap<String, String> formData, Model model) {
         if (formData.get("field") == null) {
@@ -68,5 +81,7 @@ public class Query13Controller {
         model.addAttribute("ogButtonFlag", formData.get("field").contains("OGBTN"));
         model.addAttribute("oghButtonFlag", formData.get("field").contains("OGHBTN"));
     }
+
+
 
 }
