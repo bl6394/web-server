e952969a1395e555b9dda0ba178680154a9d10c4
bl6394 <bjorn.loftis@gmail.com>
2019-07-28 22:10:28 -0500

    adding error controller / page

---
+++ src/main/java/edu/wustl/elexicon/webserver/web/controller/CustomErrorController.java
@@ -0,0 +1,23 @@
+package edu.wustl.elexicon.webserver.web.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model){
+        model.addAttribute("errorMessage", "Something went wrong.  If this continues - please notify the administrator");
+        return "generalerror";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
