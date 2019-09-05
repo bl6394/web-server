e404091551a1a6dee2a02db2dc5a067777718115
bl6394 <bjorn.loftis@gmail.com>
2019-09-04 22:58:32 -0500

    Fixed Defects: Adding error code to page

--- src/main/java/edu/wustl/elexicon/webserver/web/controller/CustomErrorController.java
+++ src/main/java/edu/wustl/elexicon/webserver/web/controller/CustomErrorController.java
@@ -22,6 +22,7 @@ public class CustomErrorController implements ErrorController {
         String trxID = (sessionTrxId == null) ? "UNKNOWN" : sessionTrxId;
         logger.error("Session Id: " + trxID + " Error: " + exception.getMessage());
         model.addAttribute("errorMessage", "Something went wrong.  If this continues - please notify the administrator.");
+        model.addAttribute("trxId", trxID);
         return "generalerror";
     }
 
