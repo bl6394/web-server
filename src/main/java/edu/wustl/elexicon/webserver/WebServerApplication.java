b075b0aaf49a3e0ae7bae296c0abe564eb317664
bl6394 <bjorn.loftis@gmail.com>
2019-09-16 08:23:19 -0500

    Fixed Tomcat Config

--- src/main/java/edu/wustl/elexicon/webserver/WebServerApplication.java
+++ src/main/java/edu/wustl/elexicon/webserver/WebServerApplication.java
@@ -1,16 +1,7 @@
 package edu.wustl.elexicon.webserver;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebServerApplication {
--- src/main/java/edu/wustl/elexicon/webserver/configuration/TomcatConfig.java
+++ src/main/java/edu/wustl/elexicon/webserver/configuration/TomcatConfig.java
@@ -5,32 +5,13 @@ import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TomcatConfig {
-
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-        TomcatEmbeddedServletContainerFactory tomcat =
-                new TomcatEmbeddedServletContainerFactory() {
-
-                    @Override
-                    protected void postProcessContext(Context context) {
-                        SecurityConstraint securityConstraint = new SecurityConstraint();
-                        securityConstraint.setUserConstraint("CONFIDENTIAL");
-                        SecurityCollection collection = new SecurityCollection();
-                        collection.addPattern("/*");
-                        securityConstraint.addCollection(collection);
-                        context.addConstraint(securityConstraint);
-                    }
-                };
-        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
-        return tomcat;
-    }
 
     @Value("${server.port.http}")
     private int serverPortHttp;
@@ -38,14 +19,32 @@ public class TomcatConfig {
     @Value("${server.port}")
     private int serverPortHttps;
 
-    private Connector createHttpConnector() {
-        Connector connector =
-                new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(
+                TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setSecure(false);
         connector.setPort(serverPortHttp);
+        connector.setSecure(false);
         connector.setRedirectPort(serverPortHttps);
         return connector;
     }
+
 
 }
