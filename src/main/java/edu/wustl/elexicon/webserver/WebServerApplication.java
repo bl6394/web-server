e8836dfd39280fba0ff79904c298f5f352955844
bl6394 <bjorn.loftis@gmail.com>
2019-09-15 20:15:20 -0500

    Async Query 18

--- src/main/java/edu/wustl/elexicon/webserver/WebServerApplication.java
+++ src/main/java/edu/wustl/elexicon/webserver/WebServerApplication.java
@@ -1,12 +1,31 @@
 package edu.wustl.elexicon.webserver;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServerApplication.class, args);
+	}
+
+
+	//Delete once certificate ready
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(80);
+		return connector;
 	}
 }
